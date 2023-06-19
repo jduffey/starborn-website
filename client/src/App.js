93a52007cfbf11eb80a27a2eb0f5c7c66eaed7e3
Jed Duffey <jedduffey@gmail.com>
2023-06-19 13:28:34 -0400

    Color palette is displayed under the stats

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import ArtPiece from './ArtPiece';
+import ArtDigest from './ArtDigest';
+import ColorPalette from './ColorPalette';
+import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Starborn Website</h1>
-            <ArtPiece />
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            artDigest: null,
+        };
+    }
+
+    componentDidMount() {
+        const randomHash = [...Array(64)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+        const artDigest = new ArtDigest(randomHash);
+        this.setState({ artDigest });
+    }
+
+    render() {
+        const { artDigest } = this.state;
+
+        return (
+            <div className="app">
+                {artDigest && (
+                    <div>
+                        <h1>Art Digest</h1>
+                        <p>Digest: {artDigest.digest}</p>
+                        <p>Star Rank: {artDigest.starRank}</p>
+                        <p>Art Style: {artDigest.artStyle}</p>
+                        <p>Background Color: {artDigest.backgroundColor}</p>
+                        <p>Interior Colors: {artDigest.interiorColors.join(', ')}</p>
+                    </div>
+                )}
+                <ColorPalette />
+            </div>
+        );
+    }
 }
 
 export default App;
