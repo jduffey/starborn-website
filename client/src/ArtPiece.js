16f2b462981104b72ac9c5a80aa7953cf2aba70b
Jed Duffey <jedduffey@gmail.com>
2023-06-19 15:27:13 -0400

    WIP save work; working on white stroke color

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -2,47 +2,66 @@ import { colors, colorNumberMap } from './color-palette';
 
 const BORDER_WIDTH = 10;
 
-const rectangle = (x, y, width, height, fill, stroke) => {
+const allColorsAreBlack = (colors) => {
+    console.log(colors);
+    return colors.every(color => color === '#000000');
+}
+
+const getStrokeColor = (backgroundColor, patternColors) => {
+    let strokeColor = 'black';
+    if (allColorsAreBlack(patternColors)) {
+        if (backgroundColor === '#000000') {
+            strokeColor = 'white';
+        } else {
+            strokeColor = backgroundColor;
+        }
+    }
+    return strokeColor;
+}
+
+const rectangle = (x, y, width, height, fill, interiorColors) => {
     return <rect
         x={x}
         y={y}
         width={width}
         height={height}
         fill={fill}
-        stroke={stroke}
+        stroke={getStrokeColor(fill, interiorColors)}
         strokeWidth={BORDER_WIDTH}
         fillOpacity={1}
     />
 }
 
 const singleSquare = (interiorColors) => {
-    return rectangle(100, 100, 200, 200, interiorColors[0], "black");
+    return rectangle(100, 100, 200, 200, interiorColors[0], interiorColors);
 }
 
 const createTwoHorizontalBarArt = (interiorColors) => {
     return [
-        rectangle(100, 100, 200, 100, interiorColors[0], "black"),
-        rectangle(100, 200, 200, 100, interiorColors[1], "black")
+        rectangle(100, 100, 200, 100, interiorColors[0], interiorColors),
+        rectangle(100, 200, 200, 100, interiorColors[1], interiorColors)
     ];
 }
 
 const createTwoVerticalBarArt = (interiorColors) => {
     return [
-        rectangle(100, 100, 100, 200, interiorColors[0], "black"),
-        rectangle(200, 100, 100, 200, interiorColors[1], "black")
+        rectangle(100, 100, 100, 200, interiorColors[0], interiorColors),
+        rectangle(200, 100, 100, 200, interiorColors[1], interiorColors)
     ];
 }
 
 const createFourSquareArt = (interiorColors) => {
     return [
-        rectangle(100, 100, 100, 100, interiorColors[0], "black"),
-        rectangle(200, 100, 100, 100, interiorColors[1], "black"),
-        rectangle(100, 200, 100, 100, interiorColors[2], "black"),
-        rectangle(200, 200, 100, 100, interiorColors[3], "black")
+        rectangle(100, 100, 100, 100, interiorColors[0], interiorColors),
+        rectangle(200, 100, 100, 100, interiorColors[1], interiorColors),
+        rectangle(100, 200, 100, 100, interiorColors[2], interiorColors),
+        rectangle(200, 200, 100, 100, interiorColors[3], interiorColors)
     ];
 }
 
 const ArtPiece = ({ backgroundColor, artStyle, interiorColors }) => {
+    backgroundColor = "#000000";
+    interiorColors = [1, 1, 1, 1];
     const mappedColors = interiorColors.map(colorNumber => colors[colorNumberMap[colorNumber]]);
 
     return (
