6f9a3e0e7ba07998bcca34adea220bd03ba4ed01
Jed Duffey <jedduffey@gmail.com>
2023-06-19 14:55:52 -0400

    Implement the four interior design functions (single square, horizontal and vertical bars, and four square)

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -1,16 +1,43 @@
 const BORDER_WIDTH = 10;
 
-const singleSquare = (fillColor) => {
+const rectangle = (x, y, width, height, fill, stroke) => {
     return <rect
-        x={100}
-        y={100}
-        width={200}
-        height={200}
-        fill={fillColor}
-        stroke={"black"}
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        fill={fill}
+        stroke={stroke}
         strokeWidth={BORDER_WIDTH}
         fillOpacity={1}
     />
+}
+
+const singleSquare = (fillColor) => {
+    return rectangle(100, 100, 200, 200, fillColor, "black");
+}
+
+const createTwoHorizontalBarArt = (fillColor) => {
+    return [
+        rectangle(100, 100, 200, 100, fillColor, "black"),
+        rectangle(100, 200, 200, 100, fillColor, "black")
+    ];
+}
+
+const createTwoVerticalBarArt = (fillColor) => {
+    return [
+        rectangle(100, 100, 100, 200, fillColor, "black"),
+        rectangle(200, 100, 100, 200, fillColor, "black")
+    ];
+}
+
+const createFourSquareArt = (fillColor) => {
+    return [
+        rectangle(100, 100, 100, 100, fillColor, "black"),
+        rectangle(200, 100, 100, 100, fillColor, "black"),
+        rectangle(100, 200, 100, 100, fillColor, "black"),
+        rectangle(200, 200, 100, 100, fillColor, "black")
+    ];
 }
 
 const ArtPiece = ({ backgroundColor }) => {
@@ -21,7 +48,9 @@ const ArtPiece = ({ backgroundColor }) => {
                 className="art-piece"
                 style={{ backgroundColor: backgroundColor }}
             >
-                {singleSquare("pink")}
+                {/* {createTwoHorizontalBarArt("pink")} */}
+                {/* {createTwoVerticalBarArt("pink")} */}
+                {createFourSquareArt("pink")}
             </svg>
         </div>
     );
