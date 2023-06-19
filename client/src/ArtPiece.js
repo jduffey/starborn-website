abbc2a0ce3418a40c886d19651af75ff289887c5
Jed Duffey <jedduffey@gmail.com>
2023-06-19 14:38:30 -0400

    Each ArtPiece has a hardcoded pink single square box in the middle

--- client/src/ArtPiece.js
+++ client/src/ArtPiece.js
@@ -1,12 +1,23 @@
 const ArtPiece = ({ backgroundColor }) => {
+    const BORDER_WIDTH = 10;
     return (
         <div>
             <h2>Art Piece Placeholder</h2>
-            <div
+            <svg
                 className="art-piece"
                 style={{ backgroundColor: backgroundColor }}
             >
-            </div>
+                <rect
+                    x={100}
+                    y={100}
+                    width={200}
+                    height={200}
+                    fill={"pink"}
+                    stroke={"black"}
+                    strokeWidth={BORDER_WIDTH}
+                    fillOpacity={1}
+                />
+            </svg>
         </div>
     );
 }
